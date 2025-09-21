3db85e70ce803bf73d214fd8684aafb497773865
Artur <tucorodriguesloreto@hotmail.com.br>
2025-09-20 23:53:42 -0300

    Debug2

--- src/pages/LoginColab.jsx
+++ src/pages/LoginColab.jsx
@@ -10,7 +10,7 @@ function LoginColab() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://auriadb.vercel.app/api/login", {// dev: http://localhost:3000/api/logi
+      const response = await fetch("https://auria-production.up.railway.app/api/login", {// dev: http://localhost:3000/api/logi
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
