3c4870fa6f005ad754e5a87d8b1950d611559d4c
Artur <tucorodriguesloreto@hotmail.com.br>
2025-09-20 22:20:15 -0300

    BugFix: localhost

--- src/pages/LoginColab.jsx
+++ src/pages/LoginColab.jsx
@@ -10,7 +10,7 @@ function LoginColab() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/api/login", {
+      const response = await fetch("https://auria-sepia.vercel.app/api/login", {// dev: http://localhost:3000/api/logi
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
